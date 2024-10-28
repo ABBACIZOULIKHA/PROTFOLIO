98f416bf29d14d8eb808ebc176f1dc5312e0c359
ABBACIZOULIKHA <kz_abbaci@esi.dz>
2024-10-28 08:50:40 +0100

    last

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import { BrowserRouter as Router , Route , Routes } from 'react-router-dom';
-import HomePage from './pages/HomePage'
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="/PROTFOLIO">
         <Routes>
           <Route path='/' element={<HomePage />} />          
         </Routes>
